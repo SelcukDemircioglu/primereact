6d54df2e4c65105b5c37292687168070c47ae0b0
SelcukDemircioglu <57330182+SelcukDemircioglu@users.noreply.github.com>
2021-04-15 18:02:23 +0300

    plugins and onClick method
    To use and add ready-made plugins of chartjs.
    trigger like a graphic button

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -4,7 +4,7 @@ import { classNames } from '../utils/ClassNames';
 
 export class Chart extends Component {
 
-    static defaultProps = {
+     static defaultProps = {
         id: null,
         type: null,
         data: null,
@@ -12,7 +12,9 @@ export class Chart extends Component {
         width: null,
         height: null,
         style: null,
-        className: null
+        className: null,
+        plugins:null,
+        onClick:null
     }
 
     static propTypes = {
@@ -23,19 +25,19 @@ export class Chart extends Component {
         width: PropTypes.string,
         height: PropTypes.string,
         style: PropTypes.object,
-        className: PropTypes.string
+        className: PropTypes.string,
+        plugins:PropTypes.object,
+        onClick:PropTypes.func
     };
 
     initChart() {
-        import('chart.js').then((module) => {
-            if (module && module.default) {
-                this.chart = new module.default(this.canvas, {
-                    type: this.props.type,
-                    data: this.props.data,
-                    options: this.props.options
-                });
-            }
+        this.chart = new ChartJS(this.canvas, {
+            type: this.props.type,
+            data: this.props.data,
+            options: this.props.options,
+            plugins:this.props.plugins
         });
+ 
     }
 
     getCanvas() {
@@ -65,8 +67,8 @@ export class Chart extends Component {
         this.initChart();
     }
 
-    shouldComponentUpdate(nextProps) {
-        if (nextProps.data === this.props.data && nextProps.options === this.props.options && nextProps.type === this.props.type) {
+      shouldComponentUpdate(nextProps) {
+        if (nextProps.data === this.props.data && nextProps.options === this.props.options && nextProps.type === this.props.type && this.props.plugins===nextProps.plugins) {
             return false;
         }
 
@@ -95,8 +97,8 @@ export class Chart extends Component {
                 height: this.props.height
             }, this.props.style);
 
-        return (
-            <div id={this.props.id} style={style} className={className}>
+          return (
+            <div id={this.props.id} style={style} className={className} onClick={this.props.onClick}>
                 <canvas ref={(el) => {this.canvas = el;}} width={this.props.width} height={this.props.height}></canvas>
             </div>
         );
