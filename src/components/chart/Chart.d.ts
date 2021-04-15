a4e7f9b970b1e43e4aa85829eaf52f726bea28be
SelcukDemircioglu <57330182+SelcukDemircioglu@users.noreply.github.com>
2021-04-15 18:09:46 +0300

    plugins and onClick
    To use and add ready-made plugins of chartjs.
    trigger like a graphic button

--- src/components/chart/Chart.d.ts
+++ src/components/chart/Chart.d.ts
@@ -1,16 +1,17 @@
 import * as React from 'react';
 
-declare namespace Chart {
-    interface ChartProps {
-        id?: string;
-        type?: string;
-        data?: object;
-        options?: object;
-        width?: string;
-        height?: string;
-        style?: object;
-        className?: string;
-    }
+   
+interface ChartProps {
+    id?: string;
+    type?: string;
+    data?: object;
+    options?: object;
+    width?: string;
+    height?: string;
+    style?: object;
+    className?: string;
+    plugins?:object;
+    onClick?(e:  React.SyntheticEvent): void;
 }
 
 export declare class Chart extends React.Component<Chart.ChartProps, any> { }
